de2e1f5cb2fb1fa222d33aa4fcb9bab18890b19c
Edoardo Luppi <lp.edoardo@gmail.com>
2025-05-08 22:29:46 +0200

    chore: add a brief explanation to each generated charset

--- ebcdic/build.gradle.kts
+++ ebcdic/build.gradle.kts
@@ -13,10 +13,12 @@ val generateCharsets = tasks.register<GenerateCharsetTask>("generateCharsets") {
   jvmDir = layout.buildDirectory.dir("generatedCharsetsJvm")
   packageName = "com.github.lppedd.kotlinx.charset.ebcdic"
 
+  // US/Canada
   sbcs("IBM037") {
     aliases = listOf("cp037", "ibm-037", "037")
   }
 
+  // Latin 1/Open Systems
   sbcs("IBM1047") {
     aliases = listOf("cp1047", "ibm-1047", "x-IBM1047", "1047")
   }
@@ -26,17 +28,20 @@ val generateCharsets = tasks.register<GenerateCharsetTask>("generateCharsets") {
     aliases = listOf("cp273", "ibm273", "ibm-273", "273")
   }
 
-  // Austria/Germany with Euro symbol (u00A4 -> u20AC)
+  // Austria/Germany with Euro sign (like IBM273, but u00A4 -> u20AC)
   sbcs("IBM01141") {
     aliases = listOf("cp1141", "ccsid01141", "1141", "ebcdic-de-273+euro", "ibm1141", "ibm-1141")
     className = "IBM1141"
   }
 
+  // Japanese - halfwidth Katakana, fullwidth Katakana, Hiragana and Kanji
+  // Single byte portion is IBM290
   ebcdicDbcs("x-IBM930", b2Min = 0x40, b2Max = 0xFE) {
     aliases = listOf("cp930", "ibm930", "ibm-930", "930")
     className = "IBM930"
   }
 
+  // Like IBM930, but the single byte portion is IBM1041 (an extension of IBM290)
   ebcdicDbcs("x-IBM939", b2Min = 0x40, b2Max = 0xFE) {
     aliases = listOf("cp939", "ibm939", "ibm-939", "939")
     className = "IBM939"

3c75078438a0f7562f08d997444893e6ca8e1820
Edoardo Luppi <lp.edoardo@gmail.com>
2025-05-22 23:29:37 +0200

    test: add an unmappable encoding test case for IBM930

--- ebcdic/src/jsTest/kotlin/com/lppedd/kotlinx/charset/ebcdic/IBM930Test.kt
+++ ebcdic/src/jsTest/kotlin/com/lppedd/kotlinx/charset/ebcdic/IBM930Test.kt
@@ -1,4 +1,17 @@
 package com.lppedd.kotlinx.charset.ebcdic
 
+import com.lppedd.kotlinx.charset.encodeToHexString
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 @Suppress("unused")
-class IBM930Test : B2CTest(IBM930, "IBM930.b2c")
+class IBM930Test : B2CTest(IBM930, "IBM930.b2c") {
+  @Test
+  fun encodeUnmappable() {
+    var hex = IBM930.encodeToHexString("\uE843\u814A")
+    assertEquals("0E,74,5A,6F,0F", hex)
+
+    hex = IBM930.encodeToHexString("\u814A\uE843", byteArrayOf(0xFE.toByte(), 0xFE.toByte()))
+    assertEquals("FE,FE,0E,74,5A,0F", hex)
+  }
+}

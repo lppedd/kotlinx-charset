8b8c70c41e3be11ec2dfa39ab8180e3a6c562b5f
Edoardo Luppi <lp.edoardo@gmail.com>
2025-10-21 15:43:43 +0200

    test: add a proper JS test case for IBM278

---
+++ ebcdic/src/jsTest/kotlin/com/lppedd/kotlinx/charset/ebcdic/IBM278Test.kt
@@ -0,0 +1,7 @@
+// Copyright (c) 2025 Edoardo Luppi, licensed under the MIT License
+// SPDX-FileCopyrightText: 2025 Edoardo Luppi
+// SPDX-License-Identifier: MIT
+package com.lppedd.kotlinx.charset.ebcdic
+
+@Suppress("unused")
+class IBM278Test : B2CTest(IBM278, "IBM278.b2c")

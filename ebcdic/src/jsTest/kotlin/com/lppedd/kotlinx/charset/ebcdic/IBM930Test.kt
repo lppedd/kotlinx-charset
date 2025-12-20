cfb55e53fe4f4ef477eadebb66912459dd868d81
Edoardo Luppi <lp.edoardo@gmail.com>
2025-12-20 19:32:08 +0100

    fix: port fix for IBM930 from JDK-8368845
    See https://bugs.openjdk.org/browse/JDK-8368845
    and https://github.com/openjdk/jdk/pull/27594

--- ebcdic/src/jsTest/kotlin/com/lppedd/kotlinx/charset/ebcdic/IBM930Test.kt
+++ ebcdic/src/jsTest/kotlin/com/lppedd/kotlinx/charset/ebcdic/IBM930Test.kt
@@ -3,7 +3,10 @@
 // SPDX-License-Identifier: MIT
 package com.lppedd.kotlinx.charset.ebcdic
 
+import com.lppedd.kotlinx.charset.Ebcdic.SO
+import com.lppedd.kotlinx.charset.decodeToHexString
 import com.lppedd.kotlinx.charset.encodeToHexString
+import com.lppedd.kotlinx.charset.toHexString
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -16,5 +19,14 @@ class IBM930Test : B2CTest(IBM930, "IBM930.b2c") {
 
     hex = IBM930.encodeToHexString("\u0001\u814A\uE843", byteArrayOf(0xFE.toByte(), 0xFE.toByte()))
     assertEquals("01,0E,FE,FE,74,5A,0F", hex)
+  }
+
+  @Test
+  fun bugJdk8368845() {
+    var hex = IBM930.encodeToHexString("\uFF0D")
+    assertEquals("0E,42,60,0F", hex)
+
+    hex = IBM930.decodeToHexString(byteArrayOf(SO.toByte(), 0x42.toByte(), 0x60.toByte()))
+    assertEquals("\uFF0D".toHexString(), hex)
   }
 }
